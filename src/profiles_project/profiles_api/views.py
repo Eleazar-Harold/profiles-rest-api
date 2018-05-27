467f3ea203b63117a9e94aedd2b81c6fb24b8228
eleazar harold <haroldyewa@gmail.com>
2018-05-27 12:59:23 +0300

    Added POST, PUT, PATCH and DELETE Functions to our APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -34,3 +34,15 @@ class HelloAPIView(APIView):
             return Response(
                 serializer.errors, status=status.HTTP_400_BAD_REQUEST
             )
+
+    def put(self, request, pk=None):
+        """handles updating an object."""
+        return Response({'method':'put'})
+
+    def patch(self, request, pk=None):
+        """Patch request, only updates fields provided in the request."""
+        return Response({'method':'patch'})
+
+    def delete(self, request, pk=None):
+        """deletes an object."""
+        return Response({'method':'delete'})
